7bd39c6dc7948509311f756a0ff065bdc8947958
Andrea Polenta <andreapolenta@MBP-di-Andrea.fritz.box>
2020-04-14 18:47:52 +0200

    add segnalazione blast

--- routes/devices.js
+++ routes/devices.js
@@ -1,9 +1,39 @@
 var express = require('express');
+var { check, header, validationResult, checkSchema } = require('express-validator');
 var router = express.Router();
+var dbConn = require('../lib/usersDb');
 
 /* GET devices listing. */
-router.get('/', function (req, res, next) {
-    res.send('okdev')
-});
+router.post('/blast', [
+    // Authorization header must be contain
+    check('blast').exists().notEmpty().isArray(),
+], (req, res, next) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+    blasts = new Array();
+    blastsIndex = 0;
+    req.body.blast.forEach(blast => {
+        creds = Buffer.from(blast, 'base64').toString('ascii').split('|');
+        blasts[blastsIndex] = new Array(3);
+        blasts[blastsIndex][0] = creds[2];
+        blasts[blastsIndex][1] = creds[3];
+        blasts[blastsIndex][2] = creds[0].replace(/_/g, '-') + ' ' + creds[1]
+        blastsIndex++ ;
+    });
+    console.log(blasts);
+    dbConn.query('INSERT INTO blasts (my_device, enc_device, timestamp) VALUES ?', [
+        blasts
+    ], function (err, result) {
+        if (err) {
+            //errore query
+            return res.status(500).send(err);
+        } else {
+            return res.status(200).json({ "result": true })
+        }
+    });
+})
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -34,7 +34,41 @@ router.post('/login', [
       if (result[0] == undefined) {
         return res.status(401).json({ "error": "Wrong username or password" });
       } else {
-        // render to views/books/index.ejs
+        const token = auth.generateAccessToken({ user: result[0] });
+        return res.status(200).json({ "access_token": token });
+      }
+    }
+  });
+})
+
+router.post('/loginDevice', [
+  // Authorization header must be contain
+  header('authorization').exists().isString().contains("Basic ").notEmpty(),
+], (req, res, next) => {
+  console.log(req.headers);
+  // Finds the validation errors in this request and wraps them in an object with handy functions
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  creds = Buffer.from(req.headers.authorization.split(' ')[1], 'base64').toString('ascii').split(':::');
+  mac = creds[0];
+  console.log(mac)
+  password = creds[1];
+
+  dbConn.query('SELECT id, mac FROM devices WHERE mac = ? AND password = ? AND del = ?', [
+    mac,
+    password,
+    0
+  ], function (err, result) {
+    if (err) {
+      //non autorizzato
+      return res.status(500).send(err);
+    } else {
+      if (result[0] == undefined) {
+        return res.status(401).json({ "error": "Wrong mac or password" });
+      } else {
+        result[0].role = "device";
         const token = auth.generateAccessToken({ user: result[0] });
         return res.status(200).json({ "access_token": token });
       }
