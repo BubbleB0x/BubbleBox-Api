939ae511eed7b185b5ecf98c2c41018b323a7475
Andrea Polenta <andreapolenta@MBP-di-Andrea.fritz.box>
2020-04-24 20:53:52 +0200

    finish

--- routes/devices.js
+++ routes/devices.js
@@ -19,17 +19,24 @@ router.post('/blast', [
     blastsIndex = 0;
     req.body.blast.forEach(blast => {
         creds = Buffer.from(blast, 'base64').toString('ascii').split('|');
+        console.log("___CREDS____")
+        console.log(creds[3])
+        console.log("___CREDS____")
+        creds[3] = creds[3].replace("\r\n", "");
         blasts[blastsIndex] = new Array(3);
         blasts[blastsIndex][0] = creds[2];
         blasts[blastsIndex][1] = creds[3];
         blasts[blastsIndex][2] = creds[0].replace(/_/g, '-') + ' ' + creds[1]
+        console.log(blasts)
         blastsIndex++;
     });
+    console.log(blasts)
     dbConn.query('INSERT INTO blasts (my_device, enc_device, timestamp) VALUES ?', [
         blasts
     ], function (err, result) {
         if (err) {
             //errore query
+            console.log(err)
             return res.status(500).send(err);
         } else {
             return res.status(200).json({ "result": true })
